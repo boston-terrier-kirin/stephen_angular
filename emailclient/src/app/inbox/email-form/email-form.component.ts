6e9d8897bf71edc7b076cb6c24354d074d20d883
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-03-30 16:41:01 +0900

    emailclient/Section24-346

--- emailclient/src/app/inbox/email-form/email-form.component.ts
+++ emailclient/src/app/inbox/email-form/email-form.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Email } from '../models/email';
 
 @Component({
   selector: 'app-email-form',
   templateUrl: './email-form.component.html',
-  styleUrls: ['./email-form.component.css']
+  styleUrls: ['./email-form.component.css'],
 })
 export class EmailFormComponent implements OnInit {
+  @Input() email!: Email;
 
-  constructor() { }
+  controls: any;
+  form!: FormGroup;
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.controls = this.createControls();
+    this.form = new FormGroup({
+      ...this.controls,
+    });
   }
 
+  createControls() {
+    const to = new FormControl(this.email.to, [Validators.required]);
+    const from = new FormControl(this.email.from, [Validators.required]);
+    const subject = new FormControl(this.email.subject, [Validators.required]);
+    const text = new FormControl(this.email.text, [Validators.required]);
+
+    return {
+      to,
+      from,
+      subject,
+      text,
+    };
+  }
 }
