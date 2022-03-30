62d40388d2b5e1ad61b1d45389af44e364dbd415
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-03-30 18:17:45 +0900

    emailclient/Section24-356 ngOnInitは初回しか呼ばれないので、コンポーネントの値が変わるたびに初期処理をしたいのであれば、 ngOnChangeを使う。

--- emailclient/src/app/inbox/email-reply/email-reply.component.ts
+++ emailclient/src/app/inbox/email-reply/email-reply.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { EmailService } from '../email.service';
+import { Email } from '../models/email';
 
 @Component({
   selector: 'app-email-reply',
   templateUrl: './email-reply.component.html',
-  styleUrls: ['./email-reply.component.css']
+  styleUrls: ['./email-reply.component.css'],
 })
-export class EmailReplyComponent implements OnInit {
+export class EmailReplyComponent implements OnChanges {
+  @Input() email!: Email;
+  showModal = false;
 
-  constructor() { }
+  constructor(private emailService: EmailService) {}
 
-  ngOnInit(): void {
+  ngOnChanges(): void {
+    // ngOnInitは1回しか呼ばれないので、ngOnChangeに変える必要がある。
+    this.email = {
+      ...this.email,
+      from: this.email.to,
+      to: this.email.from,
+      subject: `Re: ${this.email.subject}`,
+      text: `\n\n----- ${this.email.to} wrote\n${this.email.text}`,
+    };
   }
 
+  onEmailSubmit(email: Email) {
+    this.emailService.sendEmail(email).subscribe((res) => {
+      this.showModal = false;
+    });
+  }
 }
