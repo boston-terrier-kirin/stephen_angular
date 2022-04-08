f7580d67127fab006b4f37c15a5ec6b055d6a0f7
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-09 07:30:39 +0900

    component-gallery/Section7-104 AppRoutingModuleを最後にimportする理由がやっと理解できた。

--- component-gallery/src/app/app.module.ts
+++ component-gallery/src/app/app.module.ts
@@ -5,11 +5,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CollectionsModule } from './collections/collections.module';
 import { ElementsModule } from './elements/elements.module';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ElementsModule, CollectionsModule],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent],
+  imports: [BrowserModule, ElementsModule, CollectionsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+/**
+ * ・AppRoutingModule -> ElementsModule -> CollectionsModule の順でimportした場合
+ * 　{ path: ''           ,component: HomeComponent,},
+ * 　{ path: '**'         ,component: NotFoundComponent,},
+ * 　{ path: 'elements'   ,component: ElementsHomeComponent,}
+ * 　{ path: 'collections',component: CollectionsHomeComponent,}
+ * 　⇒この順番になるので、elements/colletionsにアクセスしても、notfoundに行ってしまう。
+ *
+ * ・ElementsModule -> CollectionsModule -> AppRoutingModule の順でimportした場合
+ * 　{ path: 'elements'   ,component: ElementsHomeComponent,}
+ * 　{ path: 'collections',component: CollectionsHomeComponent,}
+ * 　{ path: ''           ,component: HomeComponent,},
+ * 　{ path: '**'         ,component: NotFoundComponent,},
+ * 　⇒この順番になるので、想定通りの動きになる。
+ */
