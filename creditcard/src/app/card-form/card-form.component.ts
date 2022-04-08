bf59a3fbf4362ad2194536f894b9cf51b0b1c220
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-08 13:40:22 +0900

    creditcard/Section18-230 dirtyだけで判定すると、入力してすぐにlengthチェックでエラーになる。 それはあんまりなので、touchedも判定に加えて、blurのタイミングでエラーになるようにする。 1回blurすると、touched済みなので、その時は即座にエラーになる。

--- creditcard/src/app/card-form/card-form.component.ts
+++ creditcard/src/app/card-form/card-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-card-form',
@@ -8,10 +8,32 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class CardFormComponent implements OnInit {
   form = new FormGroup({
-    name: new FormControl(''),
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
   });
 
-  constructor() {}
+  constructor() {
+    console.log(this.form.controls['name']);
+  }
 
   ngOnInit(): void {}
+
+  showState() {
+    const target = this.form.controls['name'];
+
+    // dirtyだけで判定すると、入力してすぐにlengthチェックでエラーになる。
+    // それはあんまりなので、touchedも判定に加えて、blurのタイミングでエラーになるようにする。
+    // 1回blurすると、touched済みなので、その時は即座にエラーになる。
+
+    // コントロールをクリックして、blurしたらtrue
+    console.log('touched: ', target.touched);
+
+    // コントロールをクリックして、まだblurしていなかったらtrue
+    console.log('untouched: ', target.untouched);
+
+    // まったく触れていない状態
+    console.log('pristine: ', target.pristine);
+
+    // 値を変えた状態
+    console.log('dirty: ', target.dirty);
+  }
 }
