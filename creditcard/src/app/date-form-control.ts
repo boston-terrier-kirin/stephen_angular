0dba78ba5c189768f294fec76862dc6d133b3ab2
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-09 03:56:28 +0900

    creditcard/Section18-240

--- creditcard/src/app/date-form-control.ts
+++ creditcard/src/app/date-form-control.ts
@@ -2,6 +2,33 @@ import { FormControl } from '@angular/forms';
 
 export class DateFormControl extends FormControl {
   override setValue(value: string, options: any) {
+    // 0-9と/以外は、今の値をそのままセットして無視する。
+    if (value.match(/[^0-9|\/]/gi)) {
+      super.setValue(this.value, {
+        ...options,
+        emitModelToViewChange: true,
+      });
+      return;
+    }
+
+    // MM/YY の桁数を超えたら、今の値をそのままセットして無視する。
+    if (value.length > 5) {
+      super.setValue(this.value, {
+        ...options,
+        emitModelToViewChange: true,
+      });
+      return;
+    }
+
+    // 12/ で今の値が3桁で、入力値が2桁の場合は許可して、backsspaceが入力できるようにする。
+    if (value.length === 2 && this.value.length === 3) {
+      super.setValue(value, {
+        ...options,
+        emitModelToViewChange: true,
+      });
+      return;
+    }
+
     if (value.length === 2) {
       super.setValue(value + '/', {
         ...options,
@@ -9,6 +36,7 @@ export class DateFormControl extends FormControl {
       });
       return;
     }
+
     super.setValue(value, {
       ...options,
       emitModelToViewChange: true,
