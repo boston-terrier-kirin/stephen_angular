655bb47f8914eb57f25bcfb7d8c3a2a417e4c265
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-11 14:24:54 +0900

    component-gallery/Section11-142

--- component-gallery/src/app/mods/modal/modal.component.ts
+++ component-gallery/src/app/mods/modal/modal.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  styleUrls: ['./modal.component.css'],
 })
-export class ModalComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class ModalComponent implements OnInit, OnDestroy {
+  constructor(private elementRef: ElementRef) {
+    // elementRefでapp-modalが取得できる。
+    // console.log(this.elementRef);
   }
 
+  ngOnInit(): void {
+    // モーダルはbody直下にしないと、バックドロップと表示位置が狂うので、
+    // elementRefでapp-modalを取得して、body直下に追加する。
+    document.body.appendChild(this.elementRef.nativeElement);
+  }
+
+  ngOnDestroy(): void {
+    this.elementRef.nativeElement.remove();
+  }
 }
--- component-gallery/src/app/views/item-list/item-list.component.ts
+++ component-gallery/src/app/views/item-list/item-list.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 export interface Item {
   image: string;
@@ -11,10 +18,23 @@ export interface Item {
   templateUrl: './item-list.component.html',
   styleUrls: ['./item-list.component.css'],
 })
-export class ItemListComponent implements OnInit {
+export class ItemListComponent implements OnInit, OnChanges, OnDestroy {
   @Input() items: Item[] = [];
 
-  constructor() {}
+  constructor() {
+    // @Inputはconstructorのタイミングでは空になっている。
+    console.log('ItemListComponent.constructor', this.items);
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log('ItemListComponent.ngOnInit', this.items);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ItemListComponent.ngOnChanges', this.items);
+  }
+
+  ngOnDestroy(): void {
+    console.log('ItemListComponent.ngOnDestroy');
+  }
 }
