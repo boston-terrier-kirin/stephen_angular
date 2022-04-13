969b4d2b1ac9bb32edf5f93f5f5ecc6e608058da
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-13 10:06:11 +0900

    w-search/172

--- w-search/src/app/search-bar/search-bar.component.ts
+++ w-search/src/app/search-bar/search-bar.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.css']
+  styleUrls: ['./search-bar.component.css'],
 })
 export class SearchBarComponent implements OnInit {
+  @Output() submitted = new EventEmitter<string>();
+  term = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  input(event: Event) {
+    this.term = (event.target as HTMLInputElement).value;
   }
 
+  submit(event: Event) {
+    event.preventDefault();
+    this.submitted.emit(this.term);
+  }
 }
